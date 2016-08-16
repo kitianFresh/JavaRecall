25fca52f86d1b6a80c2e047aebdb71883da55377
kinny <1549722424@qq.com>
2016-08-16 10:10:04 +0800

    only integers(byte,char,short,int,long) can use bitwise operator

--- JavaRecall/src/operators/BBOperator.java
+++ JavaRecall/src/operators/BBOperator.java
@@ -1,5 +1,22 @@
 package operators;
 
 public class BBOperator {
-
+	public static void main(String[] args) {
+		byte a = -128;
+		char b = 0;
+		short c = 1;
+		int d = 0;
+		long e = -1;
+		float f = 0.1f;
+		double g = 0.1;
+		boolean h = true;
+		System.out.println(~a);
+		System.out.println(~b);
+		System.out.println(~c);
+		System.out.println(~d);
+		System.out.println(~e);
+		//System.out.println(~f);
+		//System.out.println(~g);
+		//System.out.println(~h);
+	}
 }
