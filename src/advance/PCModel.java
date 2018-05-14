761dda2027035c85e7a3b515b769003cbcd94dfc
kinny <1549722424@qq.com>
2018-05-14 11:06:47 +0800

    update

--- src/advance/PCModel.java
+++ src/advance/PCModel.java
@@ -1,9 +1,8 @@
 package advance;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class PCModel {
+	
 	public static void main(String[] args) {
 		SharedObject so = new SharedObject();
 		Producer p = new Producer(so);
@@ -13,91 +12,96 @@ public class PCModel {
 		new Thread(c1, "c1").start();
 		new Thread(c2, "c2").start();
 		Thread pt = new Thread(p,"p");
-		pt.setPriority(1);
+		//pt.setPriority(1);
 		pt.start();
 		
 	}
-}
+	
+	static class SharedObject {
+		private int[] buffer = new int[64];
+		private int rindex = 0;
+		private int windex = 0;
+		private int count = 0;
 
-class SharedObject {
-	private int[] buffer = new int[64];
-	private int rindex = 0;
-	private int windex = 0;
+		public int get() {
+			synchronized(this) {
+				while (count == 0) {
+					try {
+//						System.out.println(Thread.currentThread().getName()+ " get() wait");
+						this.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				int item = this.buffer[rindex];
+				rindex = (rindex+1)%64;
+				count --;
+				this.notify();
+				return item;
+			}		
+		}
+		
+		public void put(int product) {
+			synchronized(this) {
+				while (count >= buffer.length) {
+					try {
+						this.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				this.buffer[windex] = product;
+				windex = (windex+1)%64;
+				count ++;
+				this.notifyAll();
+			}
+		}
+	}
 
-	public int get() {
-		synchronized(this) {
-			while (rindex >= windex) {
+	static class Producer implements Runnable {
+		SharedObject so = null;
+		Producer(SharedObject so) {
+			this.so = so;
+		}
+		@Override
+		public void run() {
+			int i = 0;
+			while(true){
+				so.put(++i);
+				System.out.println(Thread.currentThread().getName()+"-" + i);
 				try {
-					this.wait();
+					Thread.sleep(100);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
-			int item = this.buffer[rindex];
-			rindex = (rindex++)%64;
-			System.out.println(item);
-			this.notify();
-			System.out.println(item);
-			return item;
-		}		
+			
+		}
+		
 	}
-	
-	public void put(int product) {
-		synchronized(this) {
-			while (windex >= 64+rindex) {
+
+	static class Consumer implements Runnable {
+		SharedObject so = null;
+		Consumer(SharedObject so) {
+			this.so = so;
+		}
+		@Override
+		public void run() {
+			System.out.println(Thread.currentThread().getName() + " started");
+			while(true){
+				int item = so.get();
+				System.out.println(Thread.currentThread().getName()+"-"+item);	
 				try {
-					this.wait();
+					Thread.sleep(100);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
-			this.buffer[windex] = product;
-			windex = (windex++)%64;
-			this.notify();
-		}
-	}
-}
-
-class Producer implements Runnable {
-	SharedObject so = null;
-	Producer(SharedObject so) {
-		this.so = so;
-	}
-	@Override
-	public void run() {
-		int i = 0;
-		while(true){
-			so.put(++i);
-			System.out.println(Thread.currentThread().getName()+"-" + i);
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			
 		}
 		
 	}
-	
-}
 
-class Consumer implements Runnable {
-	SharedObject so = null;
-	Consumer(SharedObject so) {
-		this.so = so;
-	}
-	@Override
-	public void run() {
-		while(true){
-			int item = so.get();
-			System.out.println(Thread.currentThread().getName()+"-"+item);	
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		
-	}
-	
+
 }
 
